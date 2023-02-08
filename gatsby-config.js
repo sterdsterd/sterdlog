379a68d516b1effbc5e6cc6d00e787447a1d5db8
sterdsterd <sterdsterd@icloud.com>
2023-02-09 03:52:31 +0900

    Chore: Add styled-components

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,9 +9,9 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `sterdlog`,
     author: {
-      name: `Kyle Mathews`,
+      name: `Yulwon Rhee`,
       summary: `who lives and works in San Francisco building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
@@ -119,6 +119,12 @@ module.exports = {
         // theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
       },
     },
   ],
