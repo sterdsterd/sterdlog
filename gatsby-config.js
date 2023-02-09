400d9bde057651a703b968a915578a1d77783cc0
sterdsterd <sterdsterd@icloud.com>
2023-02-09 22:21:15 +0900

    Chore: Add gatsby-remark-autolink-headers

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,6 +62,7 @@ module.exports = {
               wrapperStyle: `margin-bottom: 1.0725rem`,
             },
           },
+          `gatsby-remark-autolink-headers`,
           `gatsby-remark-prismjs`,
         ],
       },
