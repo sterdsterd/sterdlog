34712fdd5c0ee452060fc0991e06fecdc3a293f1
sterdsterd <sterdsterd@icloud.com>
2023-02-10 06:56:39 +0900

    Feat: Design blog post list

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,5 +1,7 @@
 import * as React from "react"
 import { Link, graphql } from "gatsby"
+import styled from "styled-components"
+import { StaticImage } from "gatsby-plugin-image"
 
 import Bio from "../components/bio"
 import Layout from "../components/layout"
@@ -23,43 +25,99 @@ const BlogIndex = ({ data, location }) => {
 
   return (
     <Layout>
-      <ol style={{ listStyle: `none` }}>
+      <PostWrapper>
         {posts.map(post => {
           const title = post.frontmatter.title || post.fields.slug
 
           return (
-            <li key={post.fields.slug}>
-              <article
-                className="post-list-item"
-                itemScope
-                itemType="http://schema.org/Article"
-              >
-                <header>
-                  <h2>
-                    <Link to={post.fields.slug} itemProp="url">
-                      <span itemProp="headline">{title}</span>
-                    </Link>
-                  </h2>
-                  <small>{post.frontmatter.date}</small>
-                </header>
-                <section>
-                  <p
-                    dangerouslySetInnerHTML={{
-                      __html: post.frontmatter.description || post.excerpt,
+            <Link to={post.fields.slug} itemProp="url">
+              <Tile key={post.fields.slug}>
+                <article itemScope itemType="http://schema.org/Article">
+                  <StaticImage
+                    formats={["auto", "webp", "avif"]}
+                    src="../images/gatsby-icon.png"
+                    quality={100}
+                    alt="Profile picture"
+                    style={{
+                      backgroundSize: "cover",
+                      width: "100%",
+                      height: "250px",
                     }}
-                    itemProp="description"
                   />
-                </section>
-              </article>
-            </li>
+
+                  <TileContents>
+                    <header>
+                      <TileTitle itemProp="headline">{title}</TileTitle>
+                      <small>{post.frontmatter.date}</small>
+                    </header>
+                    <section>
+                      <TileDescription
+                        dangerouslySetInnerHTML={{
+                          __html: post.frontmatter.description || post.excerpt,
+                        }}
+                        itemProp="description"
+                      />
+                    </section>
+                  </TileContents>
+                </article>
+              </Tile>
+            </Link>
           )
         })}
-      </ol>
+      </PostWrapper>
     </Layout>
   )
 }
 
 export default BlogIndex
+
+const PostWrapper = styled.ol`
+  display: grid;
+  grid-gap: 1rem;
+  gap: 1rem;
+  grid-template-columns: repeat(2, 1fr);
+  margin-top: 3rem;
+  list-style: none;
+
+  @media screen and (max-width: 768px) {
+    grid-template-columns: 1fr;
+  }
+`
+
+const Tile = styled.div`
+  background-color: #fff;
+  display: flex;
+  border: 1px solid rgba(0, 0, 0, 0.15);
+  border-radius: 0.4rem;
+  overflow: hidden;
+  position: relative;
+  color: black;
+
+  &:hover h2 {
+    color: #2563e1;
+    transition: all 200ms;
+  }
+
+  &:hover img {
+    transform: scale(1.05);
+    transition: all 200ms;
+  }
+`
+
+const TileContents = styled.div`
+  padding: 2rem;
+`
+
+const TileTitle = styled.h2`
+  font-size: 1.5rem;
+  transition: all 200ms;
+  margin: 0;
+`
+
+const TileDescription = styled.p`
+  margin: 0;
+  padding-top: 1rem;
+`
 
 /**
  * Head export to define metadata for the page
