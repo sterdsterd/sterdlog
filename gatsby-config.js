d7af980acb5b332c1913b32575c05a82911e5bf3
sterdsterd <sterdsterd@icloud.com>
2023-02-20 10:36:41 +0900

    Feat: Add Favicon and PWA Support

--- gatsby-config.js
+++ gatsby-config.js
@@ -135,15 +135,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `Gatsby`,
+        name: `STERD`,
+        short_name: `STERD`,
         start_url: `/`,
         background_color: `#ffffff`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        theme_color: `#2563e1`,
+        display: `standalone`,
+        icon: `src/images/sterdlogo-black.png`, // This path is relative to the root of the site.
       },
     },
     {
