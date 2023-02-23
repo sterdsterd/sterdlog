d11616afc0e2eca0ed772a1405612b96b06c7a92
sterdsterd <sterdsterd@icloud.com>
2023-02-23 13:14:12 +0900

    Refactor: Separate Post list item into Component

---
+++ src/components/PostListItem.tsx
@@ -0,0 +1,103 @@
+import React from "react"
+import { Link } from "gatsby"
+import styled from "styled-components"
+
+const Tile = styled.div`
+  background-color: #fff;
+  display: flex;
+  border: 1px solid rgba(0, 0, 0, 0.15);
+  border-radius: 1rem;
+  box-shadow: 0 0.375rem 0.625rem rgba(0, 0, 0, 0.05);
+  overflow: hidden;
+  position: relative;
+  color: black;
+  transition: all 200ms;
+  height: 100%;
+
+  &:hover {
+    box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.1);
+  }
+
+  &:hover h2 {
+    color: #2563e1;
+    transition: all 200ms;
+  }
+
+  &:hover img {
+    transform: scale(1.05);
+    transition: all 200ms;
+  }
+`
+
+const TileContents = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 1.625rem 1.75rem;
+
+  @media screen and (max-width: 768px) {
+    align-items: center;
+    flex-direction: row;
+  }
+`
+
+const TileTitle = styled.h2`
+  font-size: 1.5rem;
+  transition: all 200ms;
+  margin: 0;
+`
+
+const TileDescription = styled.p`
+  margin: 0;
+  padding-top: 0.5rem;
+  @media screen and (max-width: 768px) {
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 2;
+    text-overflow: ellipsis;
+    overflow: hidden;
+  }
+`
+
+const Emoji = styled.p`
+  margin-top: -1rem;
+  margin-bottom: 0;
+  margin-left: -0.35rem;
+  font-size: 4rem;
+
+  @media screen and (max-width: 768px) {
+    font-size: 3.5rem;
+    margin-top: 0;
+    margin-right: 1.2rem;
+  }
+`
+
+type Props = {
+  slug: string
+  emoji: string
+  title: string
+  date: string
+  description: string
+}
+
+const PostListItem = (props: Props) => {
+  return (
+    <Link to={props.slug} itemProp="url" key={props.slug}>
+      <Tile>
+        <article itemScope itemType="http://schema.org/Article">
+          <TileContents>
+            <Emoji>{props.emoji}</Emoji>
+            <div>
+              <TileTitle itemProp="headline">{props.title}</TileTitle>
+              <small>{props.date}</small>
+              <TileDescription itemProp="description">
+                {props.description}
+              </TileDescription>
+            </div>
+          </TileContents>
+        </article>
+      </Tile>
+    </Link>
+  )
+}
+
+export default PostListItem
--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -6,6 +6,7 @@ import { StaticImage } from "gatsby-plugin-image"
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import PostListItem from "../components/PostListItem"
 
 type Props = {
   data: Queries.PostListPageQuery
@@ -33,25 +34,14 @@ const BlogIndex = ({ data, location }: Props) => {
         {posts.map(post => {
           var mdx = post.childMdx!
 
-          const title = mdx.frontmatter?.title || mdx.fields?.slug
-
           return (
-            <Link to={mdx.fields?.slug!} itemProp="url" key={mdx.fields?.slug!}>
-              <Tile>
-                <article itemScope itemType="http://schema.org/Article">
-                  <TileContents>
-                    <Emoji>{mdx.frontmatter?.emoji}</Emoji>
-                    <div>
-                      <TileTitle itemProp="headline">{title}</TileTitle>
-                      <small>{mdx.frontmatter?.date}</small>
-                      <TileDescription itemProp="description">
-                        {mdx.frontmatter?.description || mdx.excerpt}
-                      </TileDescription>
-                    </div>
-                  </TileContents>
-                </article>
-              </Tile>
-            </Link>
+            <PostListItem
+              slug={mdx.fields?.slug!}
+              emoji={mdx.frontmatter?.emoji!}
+              title={mdx.frontmatter?.title!}
+              date={mdx.frontmatter?.date!}
+              description={mdx.frontmatter?.description || mdx.excerpt!}
+            />
           )
         })}
       </PostWrapper>
@@ -70,75 +60,6 @@ const PostWrapper = styled.ol`
 
   @media screen and (max-width: 768px) {
     grid-template-columns: 1fr;
-  }
-`
-
-const Tile = styled.div`
-  background-color: #fff;
-  display: flex;
-  border: 1px solid rgba(0, 0, 0, 0.15);
-  border-radius: 1rem;
-  box-shadow: 0 0.375rem 0.625rem rgba(0, 0, 0, 0.05);
-  overflow: hidden;
-  position: relative;
-  color: black;
-  transition: all 200ms;
-  height: 100%;
-
-  &:hover {
-    box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.1);
-  }
-
-  &:hover h2 {
-    color: #2563e1;
-    transition: all 200ms;
-  }
-
-  &:hover img {
-    transform: scale(1.05);
-    transition: all 200ms;
-  }
-`
-
-const TileContents = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding: 1.625rem 1.75rem;
-
-  @media screen and (max-width: 768px) {
-    align-items: center;
-    flex-direction: row;
-  }
-`
-
-const TileTitle = styled.h2`
-  font-size: 1.5rem;
-  transition: all 200ms;
-  margin: 0;
-`
-
-const TileDescription = styled.p`
-  margin: 0;
-  padding-top: 0.5rem;
-  @media screen and (max-width: 768px) {
-    display: -webkit-box;
-    -webkit-box-orient: vertical;
-    -webkit-line-clamp: 2;
-    text-overflow: ellipsis;
-    overflow: hidden;
-  }
-`
-
-const Emoji = styled.p`
-  margin-top: -1rem;
-  margin-bottom: 0;
-  margin-left: -0.35rem;
-  font-size: 4rem;
-
-  @media screen and (max-width: 768px) {
-    font-size: 3.5rem;
-    margin-top: 0;
-    margin-right: 1.2rem;
   }
 `
 
